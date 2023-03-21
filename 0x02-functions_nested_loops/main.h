14a9c95ebcca183f5dbde1b41a1c4065717c7bba
Emmav7 <elyaakoubifatimazahra88@gmail.com>
2023-03-21 15:17:47 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,35 +1,23 @@
-#ifndef main_h
-
-        #define main_h
+#include "main.h"
 
 
 
-        int _putchar(char);
+        /**
 
-        void print_alphabet(void);
+         * main - check the code
 
-        void print_alphabet_x10(void);
+         *
 
-        int _islower(int c);
+         * Return: Always 0.
 
-        int _isalpha(int c);
+         */
 
-        int print_sign(int n);
+        int main(void)
 
-        int _abs(int);
+        {
 
-        int print_last_digit(int);
+            print_alphabet();
 
-        void jack_bauer(void);
+            return (0);
 
-        void times_table(void);
-
-        int add(int, int);
-
-        void print_to_98(int n);
-
-        void print_times_table(int n);
-
-
-
-        #endif
+        }
