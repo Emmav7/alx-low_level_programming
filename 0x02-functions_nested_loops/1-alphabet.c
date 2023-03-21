2d1fba3ab9ed4458ad2501d0c744b10e318365ef
Emmav7 <elyaakoubifatimazahra88@gmail.com>
2023-03-21 16:01:25 -0700

    insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,26 @@
-#include"main.h"
+#include "main.h"
+
 
 /**
- * print_alphabet - utilizes on the _putchar function to print
- *                 the alphabet a - z
+* print_alphabet - utilizes on the _putchar function to print
+* the alphabet a - z
 */
 
-void print_alphabet(void)
-{
-	int ch;
 
-	for (ch = 'a'; ch <= 'z'; ++ch)
-		_putchar(ch);
-	_putchar('\n');
+void print_alphabet(void)
+
+{
+
+char letter;
+
+
+
+for (letter = 'a'; letter <= 'z'; letter++)
+
+_putchar(letter);
+
+
+
+_putchar('\n');
+
 }
